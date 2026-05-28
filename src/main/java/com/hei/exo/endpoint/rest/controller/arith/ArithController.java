603633d87c3a963068ecbe4a791592d6c38b94bb
Fiderana-17 <hei.fiderana@gmail.com>
2026-05-28 08:18:46 +0300

    feat: implement all endpoint

--- src/main/java/com/hei/exo/endpoint/rest/controller/arith/ArithController.java
+++ src/main/java/com/hei/exo/endpoint/rest/controller/arith/ArithController.java
@@ -2,6 +2,8 @@ package com.hei.exo.endpoint.rest.controller.arith;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.hei.exo.service.ArithmeticService;
 
@@ -11,22 +13,42 @@ public class ArithController {
     private final ArithmeticService arithmeticService;
 
     @GetMapping("/add")
-    public int add(int a, int b) {
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return arithmeticService.add(a, b);
+    }
+
+    @PostMapping("/add")
+    public int addPost(@RequestParam int a, @RequestParam int b) {
         return arithmeticService.add(a, b);
     }
 
     @GetMapping("/subtract")
-    public int subtract(int a, int b) {
+    public int subtract(@RequestParam int a, @RequestParam int b) {
+        return arithmeticService.subtract(a, b);
+    }
+
+    @PostMapping("/subtract")
+    public int subtractPost(@RequestParam int a, @RequestParam int b) {
         return arithmeticService.subtract(a, b);
     }
 
     @GetMapping("/multiply")
-    public int multiply(int a, int b) {
+    public int multiply(@RequestParam int a, @RequestParam int b) {
+        return arithmeticService.multiply(a, b);
+    }
+
+    @PostMapping("/multiply")
+    public int multiplyPost(@RequestParam int a, @RequestParam int b) {
         return arithmeticService.multiply(a, b);
     }
 
     @GetMapping("/divide")
-    public double divide(int a, int b) {
+    public double divide(@RequestParam int a, @RequestParam int b) {
+        return arithmeticService.divide(a, b);
+    }
+
+    @PostMapping("/divide")
+    public double dividePost(@RequestParam int a, @RequestParam int b) {
         return arithmeticService.divide(a, b);
     }
 }
